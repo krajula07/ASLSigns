93d0b520475294c9177a6f64142d2bf705ca0d2f
Vamsi Krishna <vamsikvk@asu.edu>
2020-04-09 20:02:38 -0700

    Zero crossings added to train.py

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import pandas as pd
-#import numpy as np
+import numpy as np
 import glob
-from config import *
+import matplotlib.pyplot as plt
+from tsfresh.feature_extraction import feature_calculators as fc
 
 
 def reqcolumns(df):
@@ -30,21 +31,34 @@ def dataclean(df):
     df.bfill(inplace=True)
 '''
 def universal_normalize(df):
-    df['leftWrist_x'] = abs((df['leftWrist_x']-df['nose_x'])/(df['leftEye_x']-df['rightEye_x']))
-    df['rightWrist_x'] = abs((df['rightWrist_x']-df['nose_x'])/(df['leftEye_x']-df['rightEye_x']))
-    df['leftWrist_y'] = abs((df['leftWrist_y']-df['nose_y'])/(df['nose_y']-df['leftShoulder_y']))
-    df['rightWrist_y'] = abs((df['rightWrist_y']-df['nose_y'])/(df['nose_y']-df['rightShoulder_y']))
+    df.loc[:,'leftWrist_x'] = abs((df.loc[:,'leftWrist_x']-df.loc[:,'nose_x'])/(df.loc[:,'leftEye_x']-df.loc[:,'rightEye_x']))
+    df.loc[:,'rightWrist_x'] = abs((df.loc[:,'rightWrist_x']-df.loc[:,'nose_x'])/(df.loc[:,'leftEye_x']-df.loc[:,'rightEye_x']))
+    df.loc[:,'leftWrist_y'] = abs((df.loc[:,'leftWrist_y']-df.loc[:,'nose_y'])/(df.loc[:,'nose_y']-df.loc[:,'leftShoulder_y']))
+    df.loc[:,'rightWrist_y'] = abs((df.loc[:,'rightWrist_y']-df.loc[:,'nose_y'])/(df.loc[:,'nose_y']-df.loc[:,'rightShoulder_y']))
     return df
+
+def zero_crossings(df):
+    diff_lwx = np.diff(df.loc[:,'leftWrist_x'].to_numpy())    
+    zc_lwx = fc.number_crossing_m(diff_lwx,0);
+    diff_rwx = np.diff(df.loc[:,'rightWrist_x'].to_numpy());
+    zc_rwx = fc.number_crossing_m(diff_rwx,0);
+    diff_lwy = np.diff(df.loc[:,'leftWrist_y'].to_numpy());
+    zc_lwy = fc.number_crossing_m(diff_lwy,0);
+    diff_rwy = np.diff(df.loc[:,'rightWrist_y'].to_numpy())
+    zc_rwy = fc.number_crossing_m(diff_rwy,0);        
+    l = []
+    l = [zc_lwx, zc_rwx, zc_lwy, zc_rwy]
+    return l
 
 def get_features(df):
-    print(df)
-    return df
-
-
+    l = zero_crossings(df)
+    
+    return pd.DataFrame([l])
 
 def main():  # use your path
 
     buy_all_files = glob.glob("data/buy" + "/*.csv")
+    feature_matrix = pd.DataFrame()
 
     #filename = r'D:\Graduation_Courses\Mobile Computing\Assignment_2\CSV\buy\BUY_1_BAKRE.csv'
     for filename in buy_all_files:
@@ -53,9 +67,9 @@ def main():  # use your path
         #df_clean = dataclean(df_req_columns)
         df_norm = universal_normalize(df_req_columns)
         df_features = get_features(df_norm[['leftWrist_x', 'leftWrist_y', 'rightWrist_x', 'rightWrist_y']])
-        # pd.append([feature_matrix,df_features])
-        # print(df_req_columns)
+        feature_matrix = pd.concat([feature_matrix,df_features],ignore_index=True)
 
+    print(feature_matrix)
 
 if __name__ == "__main__":
     main()
